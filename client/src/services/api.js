e83d83f52519cc2e46b72f4492c73dd38209d9fe
A26D <aditisn2608@gmail.com>
2026-06-28 12:06:20 +0530

    Update API base URL to use environment variable

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3001/api",
   timeout: 10000,
-  headers: { "Content-Type": "application/json" },
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 api.interceptors.response.use(
